f1ed5b78ca129091677811af457c510de67ca7b0
Iván Parra <ivandarioparraiglesias@gmail.com>
2024-11-27 21:06:14 +0100

    Ejercicios herencia

--- Programación orientada a objetos/Herencia/024_POO_EjerciciosHerencia.py
+++ Programación orientada a objetos/Herencia/024_POO_EjerciciosHerencia.py
@@ -1,22 +1,22 @@
-"""
-Definir una clase padre llamada Vehiculo y dos clases hijas llamadas Coche y Bicicleta,
-las cuales heredan de la clase Padre Vehiculo.
-La clase padre debe tener los siguientes atributos y métodos
 
-Vehiculo (Clase Padre):
--Atributos (color, ruedas)
--Métodos ( __init__() y __str__ )
+# 1.- Definir una clase padre llamada Vehiculo y dos clases hijas llamadas Coche y Bicicleta,
+# las cuales heredan de la clase Padre Vehiculo.
+# La clase padre debe tener los siguientes atributos y métodos
 
-Coche  (Clase Hija de Vehículo) (Además de los atributos y métodos heredados de Vehículo):
--Atributos ( velocidad (km/hr) )
--Métodos ( __init__() y __str__() )
+# Vehiculo (Clase Padre):
+# -Atributos (color, ruedas)
+# -Métodos ( __init__() y __str__ )
 
-Bicicleta  (Clase Hija de Vehículo) (Además de los atributos y métodos heredados de Vehículo):
--Atributos ( tipo (urbana/montaña/etc )
--Métodos ( __init__() y __str__() )
-"""
+# Coche  (Clase Hija de Vehículo) (Además de los atributos y métodos heredados de Vehículo):
+# -Atributos ( velocidad (km/hr) )
+# -Métodos ( __init__() y __str__() )
 
-class Vehiculo():
+# Bicicleta  (Clase Hija de Vehículo) (Además de los atributos y métodos heredados de Vehículo):
+# -Atributos ( tipo (urbana/montaña/etc )
+# -Métodos ( __init__() y __str__() )
+
+
+"""class Vehiculo():
     
     #Métodos
     def __init__(self, color, ruedas):
@@ -57,5 +57,35 @@ print(coche)
 
 # Creamos un objeto de la clase hija Bicicleta
 bicicleta = Bicicleta('Blanca', 2, 'Urbano')
-print(bicicleta)
+print(bicicleta)"""
 
+# 2. - Crea una clase base llamada SerVivo con un método respirar que 
+# devuelva "Estoy respirando". 
+# Luego, crea una clase derivada llamada Animal que herede de SerVivo 
+# y añada un método moverse que devuelva "Me estoy moviendo". 
+# Finalmente, crea una clase derivada llamada Perro que herede 
+# de Animal y sobrescriba el método moverse para devolver "Estoy corriendo". 
+# Crea una instancia de Perro y muestra los resultados de los métodos.
+
+class SerVivo():
+    
+    #Métodos
+    def respirar(self):
+        return 'Estoy respirando'
+
+class Animal(SerVivo):
+    
+    #Métodos
+    def moverse(self):
+        return 'Me estoy moviendo'
+
+class Perro(Animal):
+    
+    #Métodos
+    def moverse(self):
+        return 'Estoy corriendo'
+    
+perro = Perro()
+
+print(perro.moverse())
+print(perro.respirar())
